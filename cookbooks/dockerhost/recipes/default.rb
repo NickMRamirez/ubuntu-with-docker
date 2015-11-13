e403f8bc0b804bc216398f796f2aede6e0de06d2
unknown <nramirez@HMBX1CARBON0286.hmbnet.hmb>
2015-11-13 16:28:56 -0500

    Added HAProxy Docker container

--- cookbooks/dockerhost/recipes/default.rb
+++ cookbooks/dockerhost/recipes/default.rb
@@ -16,10 +16,12 @@ end
 
 cookbook_file '/home/vagrant/myweb/index.rb' do
   source 'index.rb'
+  notifies :run, 'execute[start_sinatra]', :immediately
 end
 
 execute 'start_sinatra' do
-  command 'sudo docker run -p 80:80 -v /home/vagrant/myweb:/usr/src/app -e MAIN_APP_FILE=index.rb -d erikap/ruby-sinatra'
+  action :nothing
+  command 'sudo docker run -p 81:80 --name web1 -v /home/vagrant/myweb:/usr/src/app -e MAIN_APP_FILE=index.rb -d erikap/ruby-sinatra'
 end
 
 
@@ -31,8 +33,22 @@ end
 
 cookbook_file '/home/vagrant/myweb2/index.rb' do
   source 'index2.rb'
+  notifies :run, 'execute[start_sinatra2]', :immediately
 end
 
-execute 'start_sinatra' do
-  command 'sudo docker run -p 81:80 -v /home/vagrant/myweb2:/usr/src/app -e MAIN_APP_FILE=index.rb -d erikap/ruby-sinatra'
+execute 'start_sinatra2' do
+  action :nothing
+  command 'sudo docker run -p 82:80 --name web2 -v /home/vagrant/myweb2:/usr/src/app -e MAIN_APP_FILE=index.rb -d erikap/ruby-sinatra'
+end
+
+directory '/home/vagrant/haproxy'
+
+cookbook_file '/home/vagrant/haproxy/haproxy.cfg' do
+  source 'haproxy.cfg'
+  notifies :run, 'execute[start_haproxy]', :immediately
+end
+
+execute 'start_haproxy' do
+  action :nothing
+  command 'sudo docker run -d -p 80:80 --name lb1 --link web1:web1 --link web2:web2 -v /home/vagrant/haproxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg haproxy'
 end
