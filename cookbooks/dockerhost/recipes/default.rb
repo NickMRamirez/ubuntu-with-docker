911d45e0f1d5b6ff5bf80abe03207f70d3c8764c
NickMRamirez <nickram44@gmail.com>
2015-11-14 11:07:58 -0500

    Changed docker containers to use docker network instead of links

--- cookbooks/dockerhost/recipes/default.rb
+++ cookbooks/dockerhost/recipes/default.rb
@@ -6,6 +6,12 @@
 
 docker_service 'default' do
   action [:create, :start]
+  notifies :run, "execute[docker_network]", :immediately
+end
+
+execute 'docker_network' do
+  action :nothing
+  command "sudo docker network create --driver bridge private_nw"
 end
 
 webserver_count = 2
@@ -24,7 +30,7 @@ webserver_count = 2
 
   execute "start_sinatra#{count}" do
     action :nothing
-    command "sudo docker run -p 8#{count}:80 --name web#{count} -v /home/vagrant/myweb#{count}:/usr/src/app -e MAIN_APP_FILE=index.rb -d erikap/ruby-sinatra"
+    command "sudo docker run -p 8#{count}:80 --name web#{count} --net private_nw -v /home/vagrant/myweb#{count}:/usr/src/app -e MAIN_APP_FILE=index.rb -d erikap/ruby-sinatra"
   end
 end
 
@@ -36,10 +42,10 @@ cookbook_file '/home/vagrant/haproxy/haproxy.cfg' do
   notifies :run, 'execute[start_haproxy]', :immediately
 end
 
-haproxy_docker_cmd = "sudo docker run -d -p 80:80 --name lb1 "
-1.upto(webserver_count) do |count|
-  haproxy_docker_cmd << "--link web#{count}:web#{count} "
-end
+haproxy_docker_cmd = "sudo docker run -d -p 80:80 --name lb1 --net private_nw "
+# 1.upto(webserver_count) do |count|
+  # haproxy_docker_cmd << "--link web#{count}:web#{count} "
+# end
 
 haproxy_docker_cmd << '-v /home/vagrant/haproxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg haproxy'
 
